b45ce432d857319baa48db7dadbb3381b8c9cded
Anurag Pandey <anurag.sky2565@gmail.com>
2026-08-26 15:08:24 +0530

    basic chat

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from codeassist!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import argparse
 
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("OPENROUTER_API_KEY not found in environment variables.")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [
+        {"role": "user", "content": args.user_prompt},
+    ]
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+    )
+
+    if response.usage is None:
+        raise RuntimeError("Tokens not found in response.")
+
+    if parser.parse_args().verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+    
+    print(f"Response: {response.choices[0].message.content}")
 
 if __name__ == "__main__":
     main()
