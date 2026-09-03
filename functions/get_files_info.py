09d1c7287c2cd3275cce07701aeeb1ce4d19fbd7
anuragpandey4 <anurag.sky2565@gmail.com>
2026-09-03 11:04:27 +0000

    added reading functions

---
+++ functions/get_files_info.py
@@ -0,0 +1,28 @@
+import os
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+
+    # Will be True or False
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+
+    if not valid_target_dir:
+        return f'''Result for "{directory}" directory:
+        Error: Cannot list "{directory}" as it is outside the permitted working directory'''
+
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+
+    res = f"Result for current directory:\n"
+
+    for d in os.listdir(target_dir):
+        
+        file_name = d
+        file_size = os.path.getsize(os.path.join(target_dir, d))
+        is_dir = os.path.isdir(os.path.join(target_dir,d))
+
+        res += f"  - {file_name}: file_size={file_size} bytes, is_dir={is_dir}\n"
+
+    return res
--- main.py
+++ main.py
@@ -1,44 +1,48 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from openai import OpenAI
-import argparse
 
 
-def main():
+def main() -> None:
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to the LLM")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("OPENROUTER_API_KEY")
-
-    if api_key is None:
-        raise RuntimeError("OPENROUTER_API_KEY not found in environment variables.")
+    if not api_key:
+        raise RuntimeError("OPENROUTER_API_KEY environment variable not set")
 
     client = OpenAI(
         base_url="https://openrouter.ai/api/v1",
         api_key=api_key,
     )
-
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
-    args = parser.parse_args()
-
     messages = [
         {"role": "user", "content": args.user_prompt},
     ]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
 
+    generate_content(client, messages, args.verbose)
+
+
+def generate_content(client: OpenAI, messages: list, verbose: bool) -> None:
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=messages,
     )
+    if not response.usage:
+        raise RuntimeError("API response appears to be malformed")
 
-    if response.usage is None:
-        raise RuntimeError("Tokens not found in response.")
+    if verbose:
+        print("Prompt tokens:", response.usage.prompt_tokens)
+        print("Response tokens:", response.usage.completion_tokens)
+    print("Response:")
+    print(response.choices[0].message.content)
 
-    if parser.parse_args().verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage.prompt_tokens}")
-        print(f"Response tokens: {response.usage.completion_tokens}")
-    
-    print(f"Response: {response.choices[0].message.content}")
 
 if __name__ == "__main__":
     main()
